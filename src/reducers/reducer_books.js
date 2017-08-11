1804a74cd111f15aaf3fa38feea4b735aaa44f85
Maegan Womble <maegan@oseberg.io>
2017-08-11 10:50:01 -0500

    wip

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'JS rocks' },
-    { title: 'Harry Potter' },
-    { title: 'The Ultimate Cookbook' },
-    { title: 'Origami Creatures' },
+    { title: 'JS rocks', pages: 109 },
+    { title: 'Harry Potter', pages: 1045 },
+    { title: 'The Ultimate Cookbook', pages: 134 },
+    { title: 'Origami Creatures', pages: 52 },
   ];
 }
